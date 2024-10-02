e0e62fa704717487fe512b4086b12051adb5fa67
Gabriele Amato <amatogabry250@gmail.com>
2024-10-02 11:43:13 +0200

    Added deltaT

--- src/constants.hpp
+++ src/constants.hpp
@@ -1,6 +1,18 @@
 #ifndef CONSTANTS_HPP
 #define CONSTANTS_HPP
 
-namespace bd {}
+namespace bd {
+
+const float windowWidth{1600.f}; // INLINE CONSTEXPR?
+const float windowHeight{900.f};
+
+const float minimumSpeed{1000.f};
+const float maximumSpeed{2000.f};
+
+const float shapeScale{3.f};
+
+const float deltaT = .001f;
+
+} // namespace bd
 
 #endif
