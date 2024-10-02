61a5eb985cf35290c51b81ced395949ac8e93384
Gabriele Amato <amatogabry250@gmail.com>
2024-10-02 11:47:59 +0200

    Implemented randomFloat, randomVelocity, rotation and toroidalSpace functions

--- src/functions.cpp
+++ src/functions.cpp
@@ -1,1 +1,47 @@
 #include "functions.hpp"
+#include "boid.hpp"
+#include "constants.hpp"
+
+#include <SFML/Graphics.hpp>
+#include <cassert>
+#include <cmath>
+#include <random>
+
+using bd::Boid;
+using vector2 = sf::Vector2f;
+
+float bd::randomFloat(float minimum, float maximum)
+{
+  std::random_device seed;
+  std::default_random_engine engine(seed());
+  std::uniform_real_distribution<float> unif(minimum, maximum);
+  float randomNumber = unif(engine);
+  assert(randomNumber >= minimum && randomNumber <= maximum);
+  return randomNumber;
+}
+
+vector2 bd::randomVelocity(float minimum, float maximum)
+{
+  float rho   = bd::randomFloat(minimum, maximum);
+  float theta = bd::randomFloat(0, 2.f * M_PIf);
+  return {rho * std::cos(theta), rho * std::sin(theta)};
+}
+
+float bd::rotation(const vector2& velocity)
+{
+  float angleRadians =
+      std::atan2(velocity.y, velocity.x) + M_PI_2f; // M_PI? è il miglior modo?
+  float angleDegrees = angleRadians * 360.f / (2.f * M_PIf);
+  return angleDegrees;
+}
+
+void bd::toroidalSpace(Boid& boid)
+{
+  vector2 position = boid.getPosition();
+  boid.setPositionX(std::fmod(position.x + bd::windowWidth, bd::windowWidth));
+  assert(boid.getPosition().x >= 0.f
+         && boid.getPosition().x <= bd::windowWidth);
+  boid.setPositionY(std::fmod(position.y + bd::windowHeight, bd::windowHeight));
+  assert(boid.getPosition().y >= 0.f
+         && boid.getPosition().y <= bd::windowHeight);
+}
--- src/functions.hpp
+++ src/functions.hpp
@@ -9,15 +9,19 @@ using boidPointers = std::vector<bd::Boid*>;
 
 namespace bd {
 
+float randomFloat(float, float);
+
+vector2 randomVelocity(float, float);
+
+float rotation(const vector2&);
+
+void toroidalSpace(Boid&);
+
 float norm(const vector2&); // meglio lavorare con il quadrato delle distanze
 
 float distance(const Boid&, const Boid&); // ritrovare distanza toroidale
 
 // non sto aggiungendo la statistica per ora
-
-float rotation(const vector2&);
-
-void toroidalSpace(vector2&);
 
 // prossimamente aggiungerò le funzioni con Cell e grid
 
@@ -28,10 +32,6 @@ vector2 alignmentVelocity(float, const boidPointers&, const Boid&);
 vector2 cohesionVelocity(float, const boidPointers&, const Boid&);
 
 void speedLimit(Boid&, float); // forse posso togliere float
-
-float randomFloat(float, float);
-
-vector2 randomVelocity(float, float);
 
 } // namespace bd
 
