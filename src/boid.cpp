1dc1f24eb6e867c022fc40ef374726588b0215fb
Gabriele Amato <amatogabry250@gmail.com>
2024-10-02 11:42:31 +0200

    Added setupShape and constructors

--- src/boid.cpp
+++ src/boid.cpp
@@ -1,9 +1,54 @@
 #include "boid.hpp"
+#include "constants.hpp"
+#include "functions.hpp"
 #include <SFML/Graphics.hpp>
+#include <cassert>
 
 using bd::Boid;
 using bd::Cell;
 using vector2 = sf::Vector2f;
+
+void Boid::setupShape(sf::Color color)
+{
+  shape_.setPointCount(4);
+  shape_.setPoint(0, vector2(0.f, 5.f));
+  shape_.setPoint(1, vector2(2.f, 0.f));
+  shape_.setPoint(2, vector2(4.f, 5.f));
+  shape_.setPoint(3, vector2(2.f, 4.f));
+  shape_.setFillColor(color);
+  shape_.setOrigin({2.f, 2.5f});
+  shape_.setScale(bd::shapeScale, bd::shapeScale);
+}
+
+Boid::Boid()
+{} // necessario?
+
+Boid::Boid(sf::Color color)
+    : position_{bd::randomFloat(0.f, bd::windowWidth),
+                bd::randomFloat(0.f, bd::windowHeight)}
+    , velocity_{bd::randomVelocity(bd::minimumSpeed, bd::maximumSpeed)}
+    , cellPointer_{nullptr}
+{
+  assert(position_.x >= 0.f
+         && position_.x
+                <= bd::windowWidth); // non so se posso mettere gli assert qua
+  assert(position_.y >= 0.f && position_.y <= bd::windowHeight);
+  assert(velocity_.x >= bd::minimumSpeed && velocity_.x <= bd::maximumSpeed);
+  assert(velocity_.y >= bd::minimumSpeed && velocity_.y <= bd::maximumSpeed);
+  Boid::setupShape(color);
+  shape_.setPosition(position_);
+  shape_.setRotation(bd::rotation(velocity_));
+}
+
+Boid::Boid(const vector2& position, const vector2& velocity,
+           const sf::Color& color)
+    : position_{position}
+    , velocity_{velocity}
+{
+  Boid::setupShape(color);
+  shape_.setPosition(position);
+  shape_.setRotation(bd::rotation(velocity));
+}
 
 vector2 Boid::getPosition() const
 {
--- src/boid.hpp
+++ src/boid.hpp
@@ -15,11 +15,11 @@ class Boid
   vector2 position_;
   vector2 velocity_;
   sf::ConvexShape shape_;
-  // Cell* cellPointer_;
+  Cell* cellPointer_;
   void setupShape(sf::Color);
 
  public:
-  //   Boid(); // potrebbe essere omesso (vedi liam)
+  Boid(); // potrebbe essere omesso (vedi liam)
   Boid(sf::Color);
   Boid(const vector2&, const vector2&, const sf::Color&);
   //   ~Boid();
@@ -27,13 +27,12 @@ class Boid
   getPosition() const; // si può facilmente accedere alle singole componenti
   vector2 getVelocity() const;
   sf::ConvexShape getShape() const;
-  //   Cell* getCellPointer() const;
+  Cell* getCellPointer() const;
   void setPosition(const vector2&);
   void setVelocity(const vector2&);
   void setPositionX(float);
   void setPositionY(float);
-  //   void setShapePosition(const vector2&); //??????
-  //   void setCellPointer(Cell*); // const? &?
+  void setCellPointer(Cell*); // const? &?
 };
 
 } // namespace bd
